14c53cb2e2b94fa37bc6a19cec412aff2d48ebeb
Gerrit Holl <g.holl@reading.ac.uk>
2017-01-19 16:12:30 +0000

    sensitivities work in progress

--- FCDR_HIRS/analysis/sensitivities.py
+++ FCDR_HIRS/analysis/sensitivities.py
@@ -51,20 +51,30 @@ a_0_func = sympy.Function("a_0")(C_s, a_1)
 a_0_fullfunc = sympy.Function("a_0")(C_s, a_1_fullfunc)
 
 T_PRT_full = sympy.Sum(d_PRTnk * C_PRTn**k, (k, 0, K-1))
-T_IWCT_full = sympy.Sum(T_PRT_full, (n, 1, N))/N
-B_full = (2*h*c**2)/(λ**5) * 1/(sympy.exp((h*c)/(λ*k_b*T_IWCT_full))-1)
-R_IWCT_full = (sympy.Integral(((ε + a_3) * B_full + (1-ε-a_3)*R_refl) * φ_fullfunc, λ)) / sympy.Integral(φ_fullfunc, λ)
-a_1_full = (R_IWCT_full + R_selfIWCT - R_selfs - a_2*(C_IWCT**2-C_s**2))/(C_IWCT-C_s)
-a_0_full = -2*C_s**2 - a_1_full*C_s
-R_e_full = a_0_full + a_1_full*C_E + a_2*C_E**2 - R_selfE
+T_IWCT_semi = sympy.Sum(T_PRT, (n, 1, N))/N
+T_IWCT_full = T_IWCT_semi.subs(T_PRT, T_PRT_full)
+#B_full = (2*h*c**2)/(λ**5) * 1/(sympy.exp((h*c)/(λ*k_b*T_IWCT_full))-1)
+B_semi = (2*h*c**2)/(λ**5) * 1/(sympy.exp((h*c)/(λ*k_b*T_IWCT))-1)
+B_full = B_semi.subs(T_IWCT, T_IWCT_full)
+R_IWCT_semi = (sympy.Integral(((ε + a_3) * B + (1-ε-a_3)*R_refl) * φ, λ)) / sympy.Integral(φ, λ)
+R_IWCT_full = R_IWCT_semi.subs(B, B_full).subs(φ, φ_fullfunc)
+#a_1_full = (R_IWCT_full + R_selfIWCT - R_selfs - a_2*(C_IWCT**2-C_s**2))/(C_IWCT-C_s)
+#a_0_full = -2*C_s**2 - a_1_full*C_s
+#R_e_full = a_0_full + a_1_full*C_E + a_2*C_E**2 - R_selfE
 
-a_1_semi = (R_IWCT_func + R_selfIWCT - R_selfs - a_2*(C_IWCT**2-C_s**2))/(C_IWCT-C_s)
-a_0_semi = -2*C_s**2 - a_1_semi*C_s
+a_1_semi = (R_IWCT + R_selfIWCT - R_selfs - a_2*(C_IWCT**2-C_s**2))/(C_IWCT-C_s)
+a_1_full = a_1_semi.subs(R_IWCT, R_IWCT_full)
+a_0_semi = -a_2*C_s**2 - a_1*C_s
+a_0_full = a_0_semi.subs(a_1, a_1_full)
 
 R_e_basic = a_0 + a_1*C_E + a_2*C_E**2 - R_selfE
-R_e_func = a_0_func + a_1_func*C_E + a_2*C_E**2 - R_selfE
-R_e_fullfunc = a_0_fullfunc + a_1_fullfunc*C_E + a_2*C_E**2 - R_selfE
-R_e_semi = a_0_semi + a_1_semi*C_E + a_2*C_E**2 - R_selfE
+R_e_func = R_e_basic.subs(a_0, a_0_func).subs(a_1, a_1_func)
+#R_e_func = a_0_func + a_1_func*C_E + a_2*C_E**2 - R_selfE
+R_e_fullfunc = R_e_basic.subs(a_0, a_0_fullfunc).subs(a_1, a_1_fullfunc)
+#R_e_fullfunc = a_0_fullfunc + a_1_fullfunc*C_E + a_2*C_E**2 - R_selfE
+#R_e_semi = a_0_semi + a_1_semi*C_E + a_2*C_E**2 - R_selfE
+R_e_semi = R_e_basic.subs(a_0,a_0_semi).subs(a_1, a_1_semi)
+R_e_full = R_e_basic.subs(a_0,a_0_full).subs(a_1, a_1_full)
 
 def print_and_write(eq, fn):
     eq = eq.simplify()
@@ -93,47 +103,64 @@ def main():
     print("=")
     sympy.pprint(R_e_full)
 
-#    for v in (C_E, C_s, C_IWCT, φ, C_PRTn):
-#        print("∂R_e/∂{!s} =".format(v))
-#        sympy.pprint(sympy.diff(R_e_basic, v))
-#        print("=")
-#        sympy.pprint(sympy.diff(R_e_func, v))
-#        print("=")
-#        sympy.pprint(sympy.diff(R_e_fullfunc, v))
-#        print("=")
-#        sympy.pprint(sympy.diff(R_e_semi, v))
-#        print("=")
-#        sympy.pprint(sympy.diff(R_e_full, v))
-#        print("=")
-#        sympy.pprint(sympy.diff(a_0_full + a_1_full*C_E + a_2*C_E**2, v))
     print("∂R_e/∂C_E =")
     print_and_write(sympy.diff(R_e_basic, C_E), outdir / "C_e.html")
-#    sympy.pprint(sympy.diff(R_e_basic, C_E))
-#     print("=")
-#     sympy.pprint(sympy.diff(a0a + a1b*C_E + a_2*C_E**2, C_E))
-#     print("=")
-#     sympy.pprint(sympy.diff(a0a + a_1*C_E + a_2*C_E**2, C_E))
     print("∂R_e/∂C_s =")
-#     sympy.pprint(sympy.diff(a0a + a1a*C_E + a_2*C_E**2, C_s))
-#     print("=")
-    print_and_write(sympy.diff(R_e_semi, C_s), outdir / "C_s.html")
-#     print("=")
-#     sympy.pprint(sympy.diff(a0a + a_1*C_E + a_2*C_E**2, C_s))
+    print_and_write(sympy.diff(
+        R_e_basic.subs(
+            a_0, a_0_semi).subs(
+            a_1, a_1_semi),
+        C_s), outdir / "C_s.html")
     print("∂R_e/∂C_IWCT =")
-    print_and_write(sympy.diff(R_e_semi, C_IWCT), outdir / "C_IWCT.html")
-#    sympy.pprint(sympy.diff(R_e_semi, C_IWCT).simplify())
-#     print("=")
-#     sympy.pprint(sympy.diff(a0a + a1b*C_E + a_2*C_E**2, C_IWCT))
-#     print("=")
-#     sympy.pprint(sympy.diff(a0a + a_1*C_E + a_2*C_E**2, C_IWCT))
-#     print("∂R_e/∂φ =")
-#     sympy.pprint(sympy.diff(a0a + a1a*C_E + a_2*C_E**2, φ))
-#     print("=")
-#     sympy.pprint(sympy.diff(a0a + a1b*C_E + a_2*C_E**2, φ))
-#     print("=")
-#     sympy.pprint(sympy.diff(a0a + a_1*C_E + a_2*C_E**2, φ))
-#     #pass
+    print_and_write(sympy.diff(
+        R_e_basic.subs(
+            a_0, a_0_semi).subs(
+                a_1, a_1_semi),
+        C_IWCT), outdir / "C_IWCT.html")
     print("∂R_e/∂C_PRTn =")
-#    sympy.pprint(sympy.diff(R_e_full, C_PRTn).simplify())
     print_and_write(sympy.diff(R_e_full, C_PRTn), outdir / "C_PRTn.html")
-#    print_and_write(sympy.diff(R_e_full, C_IWCT), "C_IWCT.html")
+    print("∂R_e/∂d_PRTnk =")
+    print_and_write(sympy.diff(R_e_full, d_PRTnk), outdir / "d_PRTnk.html")
+    # FIXME: should be able to automatically do this, see
+    # https://github.com/FIDUCEO/FCDR_HIRS/issues/41
+    print("∂R_e/∂T_IWCT =")
+    print_and_write(sympy.diff(
+        R_e_basic.subs(
+            a_0, a_0_semi).subs(
+            a_1, a_1_semi).subs(
+            R_IWCT, R_IWCT_semi).subs(
+            B, B_semi),
+        T_IWCT), outdir / "T_IWCT.html")
+    print("∂R_e/∂T_IWCT =")
+    print_and_write(sympy.diff(
+        R_e_basic.subs(
+            a_0, a_0_semi).subs(
+            a_1, a_1_semi).subs(
+            R_IWCT, R_IWCT_semi).subs(
+            B, B_semi).subs(
+            T_IWCT, T_IWCT_semi).subs(
+            φ, φ_func),
+        T_PRT), outdir / "T_PRT.html")
+    print("∂R_e/∂a_3 =")
+    print_and_write(sympy.diff(
+        R_e_basic.subs(
+            a_0, a_0_semi).subs(
+            a_1, a_1_semi).subs(
+            R_IWCT, R_IWCT_semi).subs(
+            φ, φ_func),
+        a_3), outdir / "a_3.html")
+    print("∂R_e/∂a_2 =")
+    print_and_write(sympy.diff(
+        R_e_basic.subs(
+            a_0, a_0_semi).subs(
+            a_1, a_1_semi),
+        a_2), outdir / "a_2.html")
+    print("∂R_e/∂R_refl =")
+    print_and_write(sympy.diff(
+        R_e_basic.subs(
+            a_0, a_0_semi).subs(
+            a_1, a_1_semi).subs(
+            R_IWCT, R_IWCT_semi).subs(
+            φ, φ_func),
+        R_refl), outdir / "R_refl.html")
+
--- README.md
+++ README.md
@@ -9,6 +9,7 @@ After installation, command-line utilities include:
     plot_hirs_temp_matrix
     plot_hirs_calibcounts_per_scanpos
     plot_hirs_test_rself
+    calc_sensitivity_params
 
 Each of those have an online help, i.e. plot_hirs_field_timeseries --help,
 listing all the options and capabilities.
