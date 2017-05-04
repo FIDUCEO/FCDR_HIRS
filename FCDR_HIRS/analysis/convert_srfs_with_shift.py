eba82b636b643512c0623b093c1eb63aa632fa7b
Gerrit Holl <g.holl@reading.ac.uk>
2017-05-04 15:30:24 +0100

    Also write out HIRS2 SRFs with uncertainties
    Still contains a bug, see #101

--- FCDR_HIRS/analysis/convert_srfs_with_shift.py
+++ FCDR_HIRS/analysis/convert_srfs_with_shift.py
@@ -10,22 +10,25 @@ now = datetime.datetime.now
 
 from typhon.physics.units.em import SRF
 from typhon.physics.units.common import ureg
-from typhon.datasets.tovs import (HIRS3, HIRS4)
+from typhon.datasets.tovs import (HIRS2, HIRS3, HIRS4)
 
 outdir = pathlib.Path("/group_workspaces/cems2/fiduceo/scratch/HIRS_SRF")
 satname = sys.argv[1].upper()
 
 
 def main():
+    # FIXME: consider the channels that have changed positions separately,
+    # such as channel 10
     std_centroid_per_ch_nm = {
         no+1: numpy.std(
-            [SRF.fromArtsXML(satname.upper(), "hirs", no+1
+            [SRF.fromArtsXML(satname.upper().replace("A0", "A"), "hirs", no+1
                             ).centroid().to("nm", "sp").m
-                for satname in HIRS3.satellites.keys() | HIRS4.satellites.keys()])
+                for satname in HIRS2.satellites.keys() | HIRS3.satellites.keys() | HIRS4.satellites.keys()
+                if not satname.endswith("13")])
             for no in range(19)}
     (outdir / satname.lower()).mkdir(parents=True, exist_ok=True)
     for ch in range(1, 20):
-        srf = SRF.fromArtsXML(satname, "hirs", ch)
+        srf = SRF.fromArtsXML(satname.replace("A0", "A"), "hirs", ch)
 
         std = std_centroid_per_ch_nm[ch]
         for shift in ureg.Quantity(numpy.linspace(-std, std, 7), ureg.nm):
