5f65c8d09c3c70fb67632d71f6e6010df8a0a8d1
Gerrit Holl <g.holl@reading.ac.uk>
2017-05-14 14:07:51 +0100

    some rpogress in preparing harm data

--- FCDR_HIRS/processing/combine_matchups.py
+++ FCDR_HIRS/processing/combine_matchups.py
@@ -31,6 +31,7 @@ logging.basicConfig(
             "%(lineno)s: %(message)s"),
     level=logging.DEBUG if p.verbose else logging.INFO)
 
+import itertools
 import datetime
 
 import numpy
@@ -56,7 +57,8 @@ class HIRSMatchupCombiner(matchups.HIRSMatchupCombiner):
         else:
             raise RuntimeError("Onmogelĳk.  Impossible.  Unmöglich.")
         #
-        keep = {"collocation", "channel", "calibrated_channel"}
+        keep = {"collocation", "channel", "calibrated_channel",
+                "matchup_count"}
         p_ds.rename(
             {nm: "{:s}_{:s}".format(self.prim, nm)
                 for nm in p_ds.keys()
@@ -83,6 +85,35 @@ class HIRSMatchupCombiner(matchups.HIRSMatchupCombiner):
             ])
         return ds
 
+    def ds2harm(self, ds, channel):
+        """Convert matchup dataset to harmonisation matchup dataset
+
+        Taking the resulf of self.as_xarray_dataset, convert it to a
+        dataset of the format wanted by the harmonisation.
+
+        Modelled on files that Jon created for AVHRR, such as on CEMS at
+        /group_workspaces/cems2/fiduceo/Data/Matchup_Simulated/Data/Harm_RealData/n17_n16.nc
+
+        See also Arta's document on the FIDUCEO wiki.
+
+        Note that one would want to merge a collection of those.
+        """
+
+        take_for_each = ("C_E", "C_IWCT", "C_s", "T_IWCT", "R_selfE")
+        take_total = list('_'.join(x) for x in itertools.product(
+                (self.prim, self.sec),
+                take_for_each))
+        da_all = [ds.sel(calibrated_channel=channel)[v]
+                    for v in take_total]
+        for (i, da) in enumerate(da_all):
+            if "calibration_position" in da.dims:
+                da_all[i] = da.median(dim="calibration_position")
+        H = xarray.concat(da_all, dim="H_labels")
+        H.name = "H_matrix"
+        H = H.assign_coords(H_labels=take_total)
+
+        raise NotImplementedError("Tot hier")
+
     def write(self, outfile):
         ds = self.as_xarray_dataset()
         logging.info("Storing to {:s}".format(
@@ -97,4 +128,6 @@ def main():
         datetime.datetime.strptime(p.to_date, p.datefmt),
         p.satname1, p.satname2)
 
-    hmc.write("/work/scratch/gholl/test.nc")
+    ds = hmc.as_xarray_dataset()
+    harm = hmc.ds2harm(ds, 12)
+    #hmc.write("/work/scratch/gholl/test.nc")
