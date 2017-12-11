c312c6eb72bce9c5b88ad361fe8cfd30b3faf905
Gerrit Holl <g.holl@reading.ac.uk>
2017-12-11 13:01:07 +0000

    gathered what goes into harmonisation, some recoding still needed

--- FCDR_HIRS/processing/combine_matchups.py
+++ FCDR_HIRS/processing/combine_matchups.py
@@ -27,7 +27,8 @@ p = parse_cmdline()
 
 # workaround for #174
 tl = dict(C_E="C_Earth",
-          fstar="f_eff")
+          fstar="f_eff",
+          R_selfE="Rself")
 
 import logging
 logging.basicConfig(
@@ -132,7 +133,7 @@ class HIRSMatchupCombiner(matchups.HIRSMatchupCombiner):
 
         As described by Sam's document on the FIDUCEO wiki:
 
-        20171108-FIDUCEO-SH-Harmonisation_Input_File_Format_Definition-v2.pdf
+        20171205-FIDUCEO-SH-Harmonisation_Input_File_Format_Definition-v6.pdf
 
         and example files at
 
@@ -144,9 +145,9 @@ class HIRSMatchupCombiner(matchups.HIRSMatchupCombiner):
         take_for_each = ["C_s", "C_IWCT", "C_E", "T_IWCT", "α", "β", "fstar", "R_selfE"]
 
         independent = {"C_E"}
-        common = {"α", "β", "fstar"}
-        structured = {"C_S", "C_IWCT", "T_IWCT", "R_selfE"}
-        wmats = {**dict.fromkeys({"C_S", "C_IWCT", "T_IWCT"}, 0),
+        u_common = {"α", "β", "fstar"}
+        structured = {"C_s", "C_IWCT", "T_IWCT", "R_selfE"}
+        wmats = {**dict.fromkeys({"C_s", "C_IWCT", "T_IWCT"}, 0),
                  **dict.fromkeys({"R_selfE"}, 1)}
 
         take_total = list('_'.join(x) for x in itertools.product(
@@ -209,18 +210,21 @@ class HIRSMatchupCombiner(matchups.HIRSMatchupCombiner):
                 ),
          "w_matrix_use2": (("m2",),
                 ),
-         "uncertainty_vector_row_count": (("uncertainty_vector_count",),
+         "u_matrix_row_count": (("u_matrix_count",),
                 ),
-         "uncertainty_vector": (("uncertainty_vector_sum_row",),
+         "u_matrix_val": (("u_matrix_row_count_sum",),
                 ),
-         "uncertainty_vector_use1": (("m1",),
+         "u_matrix_use1": (("m1",),
                 ),
-         "uncertainty_vector_use2": (("m2",),
+         "u_matrix_use2": (("m2",),
                 )}
 
         daa = xarray.merge(da_all)
 
-        # counter for w-matrices
+        # counter for uncertainties that have w-matrices.  Some but not
+        # all share the same w-matrix so the counters are different, i.e.
+        # there are more uncertainty vectors than w-matrices.
+        cc = itertools.count(1)
         for (sat, i) in ((self.prim_name, 1), (self.sec_name, 2)):
             # fill X1, X2
 
@@ -245,7 +249,7 @@ class HIRSMatchupCombiner(matchups.HIRSMatchupCombiner):
             # NB: need to tile scalar (constant) common uncertainties
             L = []
             for x in take_for_each:
-                if x in common:
+                if x in u_common:
                     da = ds.sel(calibrated_channel=channel)[f"{sat:s}_u_{tl.get(x,x):s}"]
                     L.append(
                         numpy.tile(
@@ -264,7 +268,7 @@ class HIRSMatchupCombiner(matchups.HIRSMatchupCombiner):
                 (f"m{i:d}",),
                 [1 if x in independent else
                  2 if x in structured else
-                 3 if x in common else 0
+                 3 if x in u_common else 0
                  for x in take_for_each]
                 )
 
@@ -275,10 +279,14 @@ class HIRSMatchupCombiner(matchups.HIRSMatchupCombiner):
             # fill w_matrix_use1, w_matrix_use2
             harm[f"w_matrix_use{i:d}"] = (
                 (f"m{i:d}",),
-                [2*i+wmats[x] if x in structured else 0
+                [2*i-1+wmats[x] if x in structured else 0
                     for x in take_for_each])
 
-            # fill uncertainty_vector_use1, uncertainty_vector_use2
+            # fill u_matrix_use1, u_matrix_use2
+            harm[f"u_matrix_use{i:d}"] = (
+                (f"m{i:d}",),
+                [next(cc) if x in structured else 0
+                    for x in take_for_each])
 
         # dimension matchup only:
         #
@@ -287,10 +295,6 @@ class HIRSMatchupCombiner(matchups.HIRSMatchupCombiner):
         harm["K"] = (("M",), self.kmodel.calc_K(channel))
         harm["Ks"] = (("M",), self.kmodel.calc_Ks(channel))
         harm["Kr"] = (("M",), self.krmodel.calc_Kr(channel))
-
-        # and the rest: w_matrix_nnz, w_matrix_row,
-        # w_matrix_col, w_matrix_val, uncertainty_vector_row_count,
-        # uncertainty_vector
 
         # W-matrix for C_S, C_IWCT, T_IWCT.  This should have a 1 where
         # the matchup shares the same correlation information, and a 0
@@ -311,9 +315,11 @@ class HIRSMatchupCombiner(matchups.HIRSMatchupCombiner):
         w_matrix_nnz = []
         w_matrix_col = []
         w_matrix_row = []
+        u_matrix_val = []
+        u_matrix_row_count = []
         for sat in (self.prim_name, self.sec_name):
             for dim in ("calibration_cycle", "rself_update_time"):
-                counts = numpy.unique(ds[f"{sat:s}_calibration_cycle"],
+                counts = numpy.unique(ds[f"{sat:s}_{dim:s}"],
                             return_counts=True)[1]
                 w_matrix_nnz.append((counts**2).sum())
 
@@ -325,179 +331,54 @@ class HIRSMatchupCombiner(matchups.HIRSMatchupCombiner):
 
                 # and w_matrix_row will have a form like
                 # [2 2 3 3 3 2 2]
-                w_matrix_row.append(numpy.concatenate([numpy.tile(cnt, cnt) for cnt in counts]))
+                # or is it
+                # [0 0 3 3 3 5 5] 
+                # or is it
+                # [0 2 4 7 10 13 15]
+                # ?
+                # I think it's the latter!
+                nonzero_count_per_row = numpy.concatenate([numpy.tile(cnt, cnt) for cnt in counts])
+                w_matrix_row.append(nonzero_count_per_row.cumsum()-nonzero_count_per_row[0])
+            for v in structured:
+                u = ds.sel(calibrated_channel=channel)[f"{sat:s}_u_{tl.get(x,x):s}"].values
+                u_matrix_val.extend(u)
+                u_matrix_row_count.append(len(u))
             
 
+        harm["w_matrix_nnz"] = (("w_matrix_count",),
+            numpy.array(w_matrix_nnz))
+        harm["w_matrix_col"] = (("w_matrix_nnz_sum",),
+            numpy.array(w_matrix_col))
+        harm["w_matrix_row"] = (("w_matrix_count", "w_matrix_num_row"),
+            numpy.array(w_matrix_row))
+        harm["w_matrix_val"] = (("w_matrix_nnz_sum",),
+            numpy.ones(numpy.array(w_matrix_nnz).sum()))
 
-        w_matrix_nnz = numpy.array(w_matrix_nnz)
-        w_matrix_col = numpy.array(w_matrix_col)
-        w_matrix_row = numpy.array(w_matrix_row)
-        w_matrix_val = numpy.ones(w_matrix_nnz.sum())
-
-        # W-matrix for for R_self.  One for the primary and one for the
-        # secondary.
+        harm["u_matrix_val"] = (("u_matrix_row_count_sum",),
+            numpy.array(u_matrix_val))
+        harm["u_matrix_row_count"] = (("u_matrix_count",),
+            numpy.array(u_matrix_row_count))
 
         harm = harm.assign_coords(
             m1=take_for_each,
             m2=take_for_each)
 
-        raise NotImplementedError("Tot hier hernieuwd!")
-
-        harm = xarray.Dataset(
-            {"H": H.rename({"matchup_count": "M"})},
-            coords={"m": take_total},
-            attrs={"Channel": 
-                "Ch. {:d}: {:.6~} (primary, {:s}), {:.6~} (secondary, {:s})".format(
-                    channel,
-                    self.hirs_prim.srfs[channel-1].centroid().to("um", "sp"),
-                    self.prim,
-                    self.hirs_sec.srfs[channel-1].centroid().to("um", "sp"),
-                    self.sec)})
-        harm["H"].attrs["description"] = "Inputs for harmonisation functions"
-        harm["H"].attrs["units"] = "Various"
-
-        harm["lm"] = (("L", "nl"),
-            numpy.array([[
-                typhon.datasets._tovs_defs.NOAA_numbers[self.hirs_prim.satname],
-                 typhon.datasets._tovs_defs.NOAA_numbers[self.hirs_sec.satname],
-                 harm.dims["M"]]]))
-        harm["lm"].attrs["description"] = ("Harmonisation satellite "
-            "numbers and number of entries")
-
-        # this just to fill it up so I can write to it more specifically
-        # later
-        harm["Ur"] = (harm["H"].dims, numpy.zeros_like(harm["H"].values))
-        harm["Us"] = (harm["H"].dims, numpy.zeros_like(harm["H"].values))
-        u_trans = dict(C_s="C_space", R_selfE="Rself", fstar="f_eff",
-                       C_E="C_Earth")
-
-        for sat in (self.prim, self.sec):
-            for v in take_for_each:
-                dest = "Ur" if v == "C_E" else "Us"
-                try:
-                    harm[dest].loc[{"m": "{:s}_{:s}".format(sat, v)}] = ds[
-                        "{:s}_u_{:s}".format(sat, u_trans.get(v,v))].sel(
-                            calibrated_channel=channel)
-                except KeyError as e:
-                    warnings.warn("No uncertainty defined for "
-                        "{:s}: {:s}".format(v, e.args[0]),
-                        UserWarning)
-
-        harm["Ur"].attrs["description"] = ("Random error in elements "
-            "of H")
-
-        harm["Us"].attrs["description"] = ("Nonrandom error in elements "
-            "of H")
-
-        harm["CorrIndexArray"] = (("M",), harm["{:s}_calibration_cycle".format(self.prim)])
-        harm["CorrIndexArray"].attrs["description"] = ("time at which "
-            "calibration for this measurement was performed (slave)")
-        harm["ref_CorrIndexArray"] = (("M",), harm["{:s}_calibration_cycle".format(self.sec)])
-        harm["CorrIndexArray"].attrs["description"] = ("time at which "
-            "calibration for this measurement was performed (reference)")
-
-        ref_bt = self.hirs_prim.srfs[channel-1].channel_radiance2bt(
-            ureg.Quantity(
-                ds["{:s}_R_e".format(self.prim)].sel(
-                    calibrated_channel=channel).values,
-                rad_u["si"]))
-
-        # the "slave" BT should be the radiance of the REFERENCE using the
-        # SRF of the SECONDARY, so that all other effects are nada.
-        slave_bt = self.hirs_sec.srfs[channel-1].channel_radiance2bt(
-            ureg.Quantity(
-                ds["{:s}_R_e".format(self.prim)].sel(
-                    calibrated_channel=channel).values,
-                rad_u["si"]))
-
-        harm["K_InputData"] = (("M",), ref_bt)
-        harm["K_InputData"].attrs["description"] = "Reference BT"
-        harm["K_InputData"].attrs["units"] = "K"
-        harm["K_Auxilliary"] = (("nsrfAux",),
-            [ds["{:s}_α".format(self.prim)].sel(calibrated_channel=channel),
-             ds["{:s}_β".format(self.prim)].sel(calibrated_channel=channel),
-             ds["{:s}_fstar".format(self.prim)].sel(calibrated_channel=channel)])
-        harm["K_Auxilliary"].attrs["description"] = ("Band correction "
-            "parameters, reference")
-
-        harm = harm.assign_coords(nsrfAux=["α", "β", "fstar"])
-
-        harm["corrData"] = (("ncorr",), [40])
-        harm["corrData"].attrs["description"] = "length of normal calibration cycle"
-        harm["corrData"].attrs["units"] = "Scanlines"
-
-        # to estimate K, use BT with both SRFs for ΔL
-        harm["K"] = (("M",), slave_bt - ref_bt)
         harm["K"].attrs["description"] = ("Expected ΔBT due to nominal "
             "SRF (slave - reference)")
 
-        # NB FIXME!  This should use my own BTs instead.  See #117.
-        # use local standard deviation
-        btlocal = self.ds["hirs-{:s}_bt_ch{:02d}".format(self.prim, channel)]
-        btlocal.values[btlocal>400] = numpy.nan # not all are flagged correctly
-        lsd = btlocal.loc[{"hirs-{:s}_ny".format(self.prim): slice(1, 6),
-                           "hirs-{:s}_nx".format(self.prim): slice(1, 6)}].stack(
-                    z=("hirs-{:s}_ny".format(self.prim),
-                       "hirs-{:s}_nx".format(self.prim))).std("z")
-        harm["Kr"] = (("M",), lsd)
         harm["Kr"].attrs["description"] = ("Local standard deviation "
             "in 5×5 square of HIRS around collocation")
         harm["Kr"].attrs["units"] = "K"
 
-        # propagate from band correction
-        Δ = self.hirs_sec.srfs[channel-1].estimate_band_coefficients(
-            self.hirs_sec.satname, "fcdr_hirs", channel)[-1]
-        Δ = ureg.Quantity(Δ.values, Δ.units)
-        slave_bt_perturbed = self.hirs_sec.srfs[channel-1].shift(
-            Δ).channel_radiance2bt(ureg.Quantity(
-                ds["{:s}_R_e".format(self.prim)].sel(
-                    calibrated_channel=channel).values,
-                rad_u["si"]))
-        slave_bt_perturbed_2 = self.hirs_sec.srfs[channel-1].shift(
-            Δ).channel_radiance2bt(ureg.Quantity(
-                ds["{:s}_R_e".format(self.prim)].sel(
-                    calibrated_channel=channel).values,
-                rad_u["si"]))
-        Δslave_bt = (abs(slave_bt_perturbed - slave_bt)
-                   + abs(slave_bt_perturbed_2 - slave_bt))/2
-
-        harm["Ks"] = (("M",), Δslave_bt)
         harm["Ks"].attrs["description"] = ("Propagated systematic "
             "uncertainty due to band correction factors.")
-        harm["Ks"].attrs["note"] = ("Not implemented yet, need to transfer "
-            "this when calculating FCDR and store it in debug version")
-
-        # take C_IWCT from Ur
-        harm["cal_BB_Ur"] = (("M", "nAv"),
-            harm["H"].sel(m="{:s}_C_IWCT".format(self.sec)).values.reshape(harm.dims["M"], 1))
-        harm["cal_BB_Ur"].attrs["description"] = ("IWCT counts "
-            "uncertainty (slave).  This duplicates information from Ur.")
-        # take C_space from Ur
-        harm["cal_Sp_Ur"] = (("M", "nAv"),
-            harm["H"].sel(m="{:s}_C_s".format(self.sec)).values.reshape(harm.dims["M"], 1))
-        harm["cal_Sp_Ur"].attrs["description"] = ("Space counts "
-            "uncertainty (slave).  This duplicates information from Ur.")
-
-        # same, but for reference sensor
-        harm["ref_cal_BB_Ur"] = (("M", "nAv"),
-            harm["H"].sel(m="{:s}_C_IWCT".format(self.prim)).values.reshape(harm.dims["M"], 1))
-        harm["ref_cal_BB_Ur"].attrs["description"] = ("IWCT counts "
-            "uncertainty (reference).  This duplicates information from Ur.")
-        harm["ref_cal_Sp_Ur"] = (("M", "nAv"),
-            harm["H"].sel(m="{:s}_C_s".format(self.prim)).values.reshape(harm.dims["M"], 1))
-        harm["ref_cal_Sp_Ur"].attrs["description"] = ("Space counts "
-            "uncertainty (reference).  This duplicates information from Ur.")
-
-#        for v in ("K", "Kr", "Ks", "cal_BB_Ur", "cal_Sp_Ur",
-#                  "ref_cal_BB_Ur", "ref_cal_Sp_Ur"):
-#            harm[v].attrs["note"] = "Not implemented yet, placeholder"
 
         harm.attrs["time_coverage"] = "{:%Y-%m-%d} -- {:%Y-%m-%d}".format(
-            harm[self.prim + "_time"].values[0].astype("M8[s]").astype(datetime.datetime),
-            harm[self.prim + "_time"].values[-1].astype("M8[s]").astype(datetime.datetime))
+            harm[self.prim_name + "_time"].values[0].astype("M8[s]").astype(datetime.datetime),
+            harm[self.prim_name + "_time"].values[-1].astype("M8[s]").astype(datetime.datetime))
 
-        harm.attrs["reference_satellite"] = self.prim
-        harm.attrs["slave_satellite"] = self.sec
+        harm.attrs["reference_satellite"] = self.prim_name
+        harm.attrs["slave_satellite"] = self.sec_name
 
         harm = common.time_epoch_to(
             harm,
