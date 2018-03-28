f5a84189674ac799b65175de91b571b9bf222aef
Gerrit Holl <g.holl@reading.ac.uk>
2018-03-28 15:53:49 +0100

    Started skeleton fr script to convert harm params

---
+++ FCDR_HIRS/processing/convert_harm_params.py
@@ -0,0 +1,5 @@
+"""Get harmonisation parameters and write out Python code
+"""
+
+def main():
+    raise NotImplementedError("Not implemented yet")
--- setup.py
+++ setup.py
@@ -156,6 +156,7 @@ setup(
             "hirs_orbit_map=FCDR_HIRS.analysis.map_single_orbit:main",
             "write_hirs_harm_meta=FCDR_HIRS.analysis.write_harm_meta:main",
             "merge_hirs_harmonisation=FCDR_HIRS.processing.combine_matchups:merge_files",
+            "convert_hirs_harmonisation_parameters=FCDR_HIRS.processing.convert_harm_params:main",
         ],
     },
 )
