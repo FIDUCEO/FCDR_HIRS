4f416a2481ea4cc640157082b74abac254001692
Gerrit Holl <g.holl@reading.ac.uk>
2018-11-08 13:03:28 +0000

    Correct units for S

--- FCDR_HIRS/analysis/inspect_orbit_curuc.py
+++ FCDR_HIRS/analysis/inspect_orbit_curuc.py
@@ -9,6 +9,7 @@ import argparse
 import sys
 import pathlib
 import logging
+import numbers
 
 import matplotlib
 import matplotlib.pyplot
@@ -17,7 +18,8 @@ import xarray
 
 import typhon.physics.units.em
 import typhon.datasets.tovs
-from typhon.physics.units.common import radiance_units as rad_u
+from typhon.physics.units.common import radiance_units as rad_u, ureg
+from typhon.physics.units.tools import UnitsAwareDataArray as UADA   
 
 import pyatmlab.graphics
 
@@ -59,9 +61,72 @@ def parse_cmdline():
 
     return parser.parse_args()
 
-def _S_radsi_to_K(S, srf):
-    S = S.values * rad_u["si"]**2
-    return numpy.sign(S) * numpy.sqrt(numpy.abs(S)).to("K", "radiance", srf=srf)**2
+def _S_radsi_to_K(S, srf, ref_L):
+    """Convert covariance matrix in radiance to one in BT
+
+    For cross-line and cross-element covariances, this is done by adding
+    sqrt(S) to ref_L, converting to BT, then subtracting ref_BT and
+    squaring.
+
+    For cross-channel covariances, it is more complicated.
+    In pseudocode::
+
+        S_bt[i, j] = (((ref_L[i] + u_L[i, j]).to("K") - ref_bt[i]) *
+                      ((ref_L[j] + u_L[i, j]).to("K") - ref_bt[j]))
+
+    Arguments:
+
+        S
+
+            Covariance matrix in rad_si units
+
+        srf
+
+            Either single typhon.physics.units.em.SRF or list
+            thereoff
+
+        ref_L
+
+            Reference radiances in rad_si units, relative to which the
+            convertion to S in BT will be performed
+
+    """
+    S_L = S.values * rad_u["si"]**2
+    sgn = numpy.sign(S_L)
+    u_L = numpy.sqrt(numpy.abs(S_L))
+    if isinstance(ref_L, (numbers.Number, numpy.ndarray)):
+        ref_L = ureg.Quantity(ref_L, rad_u["si"])
+    elif isinstance(ref_L, xarray.DataArray):
+        ref_L = UADA(ref_L)
+        if not "units" in ref_L.attrs.keys():
+            ref_L.attrs["units"] = rad_u["si"]
+    else:
+        raise TypeError("expected number of array for ref_L, got {!s}".format(
+            type(ref_L)))
+
+    if isinstance(srf, list): # FIXME: should be sequence more flexible
+        ref_bt = xarray.zeros_like(ref_L)
+        ref_bt.attrs["units"] = "K"
+        ref_bt.values[...] = [L.to("K", "radiance", srf=srf[i]) for (i, L) in enumerate(ref_L)]
+        u_L = UADA(
+            u_L.m,
+            attrs={"units":u_L.u},
+            dims=("calibrated_channel", "calibrated_channel"),
+            coords={"calibrated_channel": ref_L["calibrated_channel"]})
+        ref_bt = ureg.Quantity(ref_bt.values, ref_bt.attrs["units"])
+        S_bt = sgn * numpy.array(
+            [((ureg.Quantity(ref_L.values[i]+u_L.values[i,j], rad_u["si"]).to("K", "radiance", srf=srf1)-ref_bt[i]) *
+              (ureg.Quantity(ref_L.values[j]+u_L.values[i,j], rad_u["si"]).to("K", "radiance", srf=srf2)-ref_bt[j])).m
+             for (i, srf1) in enumerate(srf)
+             for (j, srf2) in enumerate(srf)]).reshape(
+                (len(srf), len(srf))) # transpose?
+        S_bt = ureg.Quantity(S_bt, ureg.K**2)
+    else: # for each channel
+        ref_bt = ref_L.to("K", "radiance", srf=srf)
+        u_bt = (ref_L + u_L).to("K", "radiance", srf=srf)-ref_bt
+        S_bt = sgn*u_bt**2
+
+    return S_bt
 
 def plot_curuc_for_pixels(ds, lines, channel, x_all, y_all):
     """Plot some CURUC stats for specific pixels
@@ -86,9 +151,7 @@ def plot_curuc_for_pixels(ds, lines, channel, x_all, y_all):
                # fail as pprint.pprint can't sort Relational objects
 
     # get centroid
-    srf = typhon.physics.units.em.SRF.fromRTTOV(
-        typhon.datasets.tovs.norm_tovs_name(ds.satellite, mode="RTTOV"),
-        "hirs", channel)
+    srf = fg.fcdr.srfs[channel-1]
     cntr = srf.centroid().to("µm", "sp")
     shared_tit = (f"{ds.satellite:s} channel {channel:d} ({cntr:.4~}) ")
     period_tit = f"{start:%Y-%m-%d %H:%M:%S} – {end:%H:%M:%S}"
@@ -115,14 +178,16 @@ def plot_curuc_for_pixels(ds, lines, channel, x_all, y_all):
         "curuc/cross_line_error_correlation_function_"+shared_fn+".png")
 
     cmap = "magma_r"
-    imshow_args = {"cmap": cmap, "interpolation": None, "origin": "upper"
+    imshow_args = {"cmap": cmap, "interpolation": None, "origin": "upper"}
     for (x, y) in zip(x_all, y_all):
+        y_new = abs(ds_new["time"]-ds.sel(y=y)["time"]).argmin().item()
         scnlinlab = "scanline at {:%Y-%m-%d %H:%M:%S}".format(
             ds["time"].isel(y=y).values.astype("M8[ms]").item())
         # cross-element error covariance matrix for line
         (f, a) = matplotlib.pyplot.subplots(1, 1, figsize=(8, 6))
         S = D["S_esΛl"][channel-1, y-lines[0], :, :]
-        S = _S_radsi_to_K(S, srf=srf)
+        S = _S_radsi_to_K(S, srf=srf,
+            ref_L=ds_new.isel(scanline_earth=y_new,scanpos=x,calibrated_channel=channel)["R_e"].item())
         #p = a.pcolor(S.m, cmap=cmap)
         p = a.imshow(S.m, **imshow_args)
         cb = f.colorbar(p)
@@ -141,7 +206,8 @@ def plot_curuc_for_pixels(ds, lines, channel, x_all, y_all):
         # cross-line error covariance matrix for element
         (f, a) = matplotlib.pyplot.subplots(1, 1, figsize=(8, 6))
         S = D["S_lsΛe"][channel-1, x, :, :]
-        S = _S_radsi_to_K(S, srf=srf)
+        S = _S_radsi_to_K(S, srf=srf,
+            ref_L=ds_new.isel(scanline_earth=y_new,scanpos=x,calibrated_channel=channel)["R_e"].item())
         #p = a.pcolor(S.m, cmap=cmap)
         p = a.imshow(S.m, **imshow_args)
         cb = f.colorbar(p)
@@ -160,9 +226,10 @@ def plot_curuc_for_pixels(ds, lines, channel, x_all, y_all):
         # cross-channel error covariance matrix
         (f, a) = matplotlib.pyplot.subplots(1, 1, figsize=(8, 6))
         S = D["S_csΛp"].sel(n_l=y-lines[0], n_e=x)
-        S = _S_radsi_to_K(S, srf=srf)
+        S = _S_radsi_to_K(S, srf=fg.fcdr.srfs,
+            ref_L=ds_new.isel(scanline_earth=y_new,scanpos=x)["R_e"])
         #p = a.pcolor(S.m, cmap=cmap)
-        p = a.imshow(S.m, **imshow_args)
+        p = a.imshow(S.m, vmax=sorted(S.m.flat)[-2], **imshow_args)
         cb = f.colorbar(p)
         cb.set_label("Covariance [K$^2$]")
         a.set_xlabel("channel")
