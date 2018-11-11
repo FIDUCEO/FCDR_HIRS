39554398c6c27f4ec356ebe81b53a9d1ac9a7050
Gerrit Holl <g.holl@reading.ac.uk>
2018-11-11 22:47:36 +0000

    more off-by-1 x-coor bug fixes

--- FCDR_HIRS/analysis/inspect_orbit_curuc.py
+++ FCDR_HIRS/analysis/inspect_orbit_curuc.py
@@ -165,7 +165,7 @@ def plot_curuc_for_pixels(ds, lines, channel, x_all, y_all):
         S = D["S_lsΛe"].sel(n_c=channel, n_e=x-1)
         # WARNING FIXME: what is the correct BT to put in?
         S = _S_radsi_to_K(S,
-             u_radK=ds_new.sel(calibrated_channel=channel).isel(scanpos=x)["u_T_b_nonrandom"].values)
+             u_radK=ds_new.sel(calibrated_channel=channel).isel(scanpos=x-1)["u_T_b_nonrandom"].values)
         #p = a.pcolor(S.m, cmap=cmap)
         p = a.imshow(S.m, **imshow_args)
         cb = f.colorbar(p)
@@ -186,7 +186,7 @@ def plot_curuc_for_pixels(ds, lines, channel, x_all, y_all):
         S = D["S_csΛp"].sel(n_l=y-lines[0], n_e=x-1)
         # WARNING FIXME: what is the correct BT to put in?
         S = _S_radsi_to_K(S,
-             u_radK=ds_new.isel(scanpos=x,scanline_earth=y_new)["u_T_b_nonrandom"].values)
+             u_radK=ds_new.isel(scanpos=x-1,scanline_earth=y_new)["u_T_b_nonrandom"].values)
         #p = a.pcolor(S.m, cmap=cmap)
         p = a.imshow(S.m, vmax=sorted(S.m.flat)[-2], **imshow_args)
         cb = f.colorbar(p)
