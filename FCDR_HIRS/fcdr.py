735a90f73d9c6bf080af3f1af88cf314817821c0
Gerrit Holl <g.holl@reading.ac.uk>
2018-11-23 16:31:31 +0000

    Also warn if ε too small

--- FCDR_HIRS/fcdr.py
+++ FCDR_HIRS/fcdr.py
@@ -505,9 +505,8 @@ class HIRSFCDR(typhon.datasets.dataset.HomemadeDataset):
         else:
             a_3 = UADA(_harm_defs.harmonisation_parameters[self.satname].get(ch, [0,0,0])[1],
                 name="correction to emissivity")
-        if a_3 > 0.02:
-            warnings.warn(f"Channel {ch:d}: ε + a₃ = {(ε+a_3):.2f} > 1!  Perhaps there is a "
-                "problem with the blackbody?", FCDRWarning)
+        if not -0.98 < a_3 < 0.02:
+            warnings.warn(f"Channel {ch:d}: ε + a₃ = {(ε+a_3):.2f}.  Problem?", FCDRWarning)
 
         # FIXME: for consistency, should replace this one also with
         # band-corrections — at least temporarily.  Perhaps this wants to
