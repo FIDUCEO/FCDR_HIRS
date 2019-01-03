6e780caddf7a7aff32f90bb85bd591e069242602
Gerrit Holl <g.holl@reading.ac.uk>
2019-01-03 16:53:40 +0000

    access ``.values`` first

--- FCDR_HIRS/matchups.py
+++ FCDR_HIRS/matchups.py
@@ -1062,8 +1062,8 @@ class KrModelLSD(KrModel):
             ok)
         ok &= functools.reduce(
             operator.and_,
-            ((self.ds_orig[f"hirs-{s:s}_bt_ch{channel:02d}"].min(f"hirs-{s:s}_nx").min(f"hirs-{s:s}_ny") > 100) &
-             (self.ds_orig[f"hirs-{s:s}_bt_ch{channel:02d}"].max(f"hirs-{s:s}_nx").max(f"hirs-{s:s}_ny") < 400)
+            (((self.ds_orig[f"hirs-{s:s}_bt_ch{channel:02d}"].min(f"hirs-{s:s}_nx").min(f"hirs-{s:s}_ny") > 100) &
+              (self.ds_orig[f"hirs-{s:s}_bt_ch{channel:02d}"].max(f"hirs-{s:s}_nx").max(f"hirs-{s:s}_ny") < 400)).values
              for s in (self.prim_name, self.sec_name)),
             ok)
         return ok
